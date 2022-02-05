e2e665098b58566c07b12d532d5a98da50b8137d
Pascal Fares <pascal.fares@cofares.net>
2022-02-05 07:57:54 +0200

    Init Client - amis -> *Client

---
+++ src/main/java/com/example/demo/model/Client.java
@@ -0,0 +1,86 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+@Entity
+@Table(schema = "paymybuddy")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String nom;
+    private String prenom;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @Fetch(value = FetchMode.JOIN)
+
+    private List<Client> friends;
+
+    public Client() {
+
+    }
+
+    public Client(String nom, String prenom, String email) {
+        this.setNom(nom);
+        this.setPrenom(prenom);
+        this.setEmail(email);
+        friends = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Client> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<Client> friends) {
+        this.friends = friends;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/test/java/com/example/demo/repository/DemoApplicationTests.java
@@ -0,0 +1,39 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Client;
+import com.example.demo.repository.ClientDAO;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Autowired
+    ClientDAO clientDao;
+
+    @Test
+    public void createAndGet() {
+        Client c = new Client("Pascal", "Fares", "pascal.fares@lecnam.net");
+        clientDao.save(c);
+        Client c1 = clientDao.findByEmail("pascal.fares@lecnam.net");
+        assertThat(c1.getNom()).isEqualTo(c.getNom());
+
+    }
+
+    @Test
+    public void createAndAddFriend() {
+        Client c = new Client("Pascal1", "Fares", "pascal1.fares@lecnam.net");
+        clientDao.save(c);
+        Client c1 = new Client("Pascal2", "Fares", "pascal2.fares@lecnam.net");  
+        c1.getFriends().add(c);
+        clientDao.save(c1);
+        Client c2 = clientDao.findByEmail("pascal2.fares@lecnam.net");
+        assertThat(c2.getFriends().size()).isEqualTo(c1.getFriends().size());
+
+    }
+    
+
+}
